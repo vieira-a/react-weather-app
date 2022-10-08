b7fde218b7115ec543a003a8619b7cb3f7b6f035
Anderson Vieira <asvieira.br@gmail.com>
2022-10-07 22:58:14 -0300

    feat: fetching geodb cities api

---
+++ src/components/Fetch.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1659ddf63dmsh3aa46963642e146p17dee4jsnc4986cd4c7de',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
